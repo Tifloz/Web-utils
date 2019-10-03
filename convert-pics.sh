924f7298607673002062ad638a8502e7273bd273
Florian Louvet <florian.louvet@epitech.eu>
2019-10-03 11:15:30 +0200

    [~] Finished the script to convert one image to all needed formats

--- convert-pics.sh
+++ convert-pics.sh
@@ -1,13 +1,40 @@
 #!/usr/bin/env bash
 
-for filename in "$1"/*png ; do
+# mobile : 1024px
+# tablet : 1300px
+# desktop : 1920px
+
+# retina : 2x
+
+SIZES[0]="1024"
+SIZES[1]="1300"
+SIZES[2]="1920"
+
+for i in "${SIZES[@]}"; do
+  if [ "$i" == "1024" ]; then
+    mkdir """$1""/mobile"
+    currentdir="""$1""/mobile"
+  fi
+  if [ "$i" == "1300" ]; then
+    mkdir """$1""/tablet"
+    currentdir="""$1""/tablet"
+  fi
+  if [ "$i" == "1920" ]; then
+    mkdir """$1""/desktop"
+    currentdir="""$1""/desktop"
+  fi
+  for filename in "$1"/*png; do
     name=${filename##*/}
-    base=${name%.png}
-    convert "$filename" -fill black -colorize 50% "$1"/tmp.png
-    cwebp -q 80 -quiet "$1"/tmp.png -o "$1/$base-2x.webp"
-    convert "$filename" -fill black -colorize 50% "$1/$base-2x.jpg"
-    convert "$filename" -fill black -colorize 50% -resize 1920x400 "$1"/tmp.png
-    cwebp -q 80 -quiet "$1"/tmp.png -o "$1/$base.webp"
-    convert "$filename" -resize 1920x400 -fill black -colorize 50% "$1/$base.jpg"
-    rm "$1"/tmp.png
+
+    if [ "$name" != "tmp.png" ]; then
+      base=${name%.png}
+      convert -monitor "$filename" "$1"/tmp.png
+      cwebp -q 80 -quiet "$1"/tmp.png -o "$currentdir/$base-2x.webp"
+      convert -monitor "$filename" "$currentdir/$base-2x.jpg"
+      convert -monitor "$filename" -resize ${i} "$1"/tmp.png
+      cwebp -q 80 -quiet "$1"/tmp.png -o "$currentdir/$base.webp"
+      convert -monitor "$filename" -resize ${i} "$currentdir/$base.jpg"
+    fi
+  done
 done
+rm "$1"/tmp.png
